aad21eba11abe4a574ba80bccca48d223875f47c
Hong Jik Lee <HongJikLee@hongs-macbook-pro-5.local>
2019-09-30 13:26:25 -0400

    Initial Commit

---
+++ StringLinkList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StringLinkList
+//
+//  Created by Hong Jik Lee on 9/30/19.
+//  Copyright © 2019 Hong Jik Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
